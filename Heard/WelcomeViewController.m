b3672d30881d72423c10adbd41ca92e89febcbcb
Bastien Beurier <bastienbeurier@gmail.com>
2014-06-20 14:57:50 -0700

    Redesign WelcomeViewController

--- Heard/WelcomeViewController.m
+++ Heard/WelcomeViewController.m
@@ -9,6 +9,11 @@
 #import "WelcomeViewController.h"
 
 @interface WelcomeViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *titbleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *subTitleLabel;
+@property (weak, nonatomic) IBOutlet UIButton *startButton;
+@property (weak, nonatomic) IBOutlet UIImageView *logoImage;
+@property (strong, nonatomic) UIView *hiddingView;
 
 @end
 
@@ -17,7 +22,36 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    self.titbleLabel.alpha = 0;
+    self.subTitleLabel.alpha = 0;
+    self.startButton.alpha = 0;
+    
+    self.hiddingView = [[UIView alloc] initWithFrame:self.logoImage.frame];
+    self.hiddingView.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:self.hiddingView];
+    
 	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    [UIView animateWithDuration:3.0 animations:^{
+        
+        self.hiddingView.frame = CGRectMake(self.hiddingView.frame.size.width,
+                                            self.hiddingView.frame.origin.y,
+                                            self.hiddingView.frame.size.width,
+                                            self.hiddingView.frame.size.height);
+    } completion:^(BOOL dummy){
+        [UIView animateWithDuration:1.0 animations:^{
+            
+            self.titbleLabel.alpha = 1;
+            self.subTitleLabel.alpha = 1;
+            self.startButton.alpha = 1;
+        }];
+    }];
 }
 
 @end
