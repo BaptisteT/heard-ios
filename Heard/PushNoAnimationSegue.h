4e0cab5ab4c56e390cff1fd0a8b5a8093405d744
Bastien Beurier <bastienbeurier@gmail.com>
2014-09-17 18:26:33 -0700

    No animation for Welcome - Dashboard push segue

---
+++ Heard/PushNoAnimationSegue.h
@@ -0,0 +1,13 @@
+//
+//  PushNoAnimationSegue.h
+//  Heard
+//
+//  Created by Bastien Beurier on 9/17/14.
+//  Copyright (c) 2014 streetshout. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PushNoAnimationSegue : UIStoryboardSegue
+
+@end
