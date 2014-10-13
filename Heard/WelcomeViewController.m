36ca47cbff48f12e9c4d60874bd13d01c10daabe
Bastien Beurier <bastienbeurier@gmail.com>
2014-10-12 19:51:25 -0700

    Welcome screen + tagline

--- Heard/WelcomeViewController.m
+++ Heard/WelcomeViewController.m
@@ -15,6 +15,8 @@
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 @property (weak, nonatomic) IBOutlet UIButton *startButton;
 @property (weak, nonatomic) IBOutlet UIImageView *logoImage;
+@property (weak, nonatomic) IBOutlet UIView *hiddingView;
+@property (weak, nonatomic) IBOutlet UITextView *subtitleTextView;
 
 @end
 
@@ -26,6 +28,34 @@
     
     [self.startButton.titleLabel sizeToFit];
     self.startButton.titleLabel.text = NSLocalizedStringFromTable(@"start_button_text",kStringFile, @"comment");
+    
+    self.titleLabel.alpha = 0;
+    self.subtitleTextView.alpha = 0;
+    self.startButton.alpha = 0;
+    self.hiddingView.hidden = NO;
+    
+    self.hiddingView.backgroundColor = [UIColor whiteColor];
+    [self.logoImage addSubview:self.hiddingView];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    [UIView animateWithDuration:3.0 animations:^{
+        
+        self.hiddingView.frame = CGRectMake(self.logoImage.bounds.size.width,
+                                            0,
+                                            self.hiddingView.bounds.size.width,
+                                            self.hiddingView.bounds.size.height);
+    } completion:^(BOOL dummy){
+        [UIView animateWithDuration:1.0 animations:^{
+            
+            self.titleLabel.alpha = 1;
+            self.subtitleTextView.alpha = 1;
+            self.startButton.alpha = 1;
+        }];
+    }];
 }
 
 - (BOOL)prefersStatusBarHidden {
