a5e3d0fce5536bc062cb213b40e7446272c264f5
Baptiste <baptiste.truchot@gmail.com>
2014-11-21 11:46:11 +0100

    bug fabrik camera utils

--- Heard/CameraUtils.m
+++ Heard/CameraUtils.m
@@ -28,37 +28,39 @@
 {
     CGFloat screenHeight = [[UIScreen mainScreen] bounds].size.height;
     CGFloat screenWidth = [[UIScreen mainScreen] bounds].size.width;
-    UIView *plCropOverlay = [[[viewController.view.subviews objectAtIndex:1]subviews] objectAtIndex:0];
-    plCropOverlay.hidden = YES;
-    int position = (screenHeight - screenWidth + 10)/2;
-    
-    CAShapeLayer *circleLayer = [CAShapeLayer layer];
-    UIBezierPath *path2 = [UIBezierPath bezierPathWithOvalInRect:
-                           CGRectMake(0.0f, position, screenWidth, screenWidth)];
-    [path2 setUsesEvenOddFillRule:YES];
-    
-    [circleLayer setPath:[path2 CGPath]];
-    
-    [circleLayer setFillColor:[[UIColor clearColor] CGColor]];
-    UIBezierPath *path = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(0, 0, screenWidth, screenHeight-72) cornerRadius:0];
-    
-    [path appendPath:path2];
-    [path setUsesEvenOddFillRule:YES];
-    
-    CAShapeLayer *fillLayer = [CAShapeLayer layer];
-    fillLayer.path = path.CGPath;
-    fillLayer.fillRule = kCAFillRuleEvenOdd;
-    fillLayer.fillColor = [UIColor blackColor].CGColor;
-    fillLayer.opacity = 0.8;
-    [viewController.view.layer addSublayer:fillLayer];
-    
-    UILabel *moveLabel = [[UILabel alloc]initWithFrame:CGRectMake(0, 10, screenWidth, 50)];
-    [moveLabel setText:@"Move and Scale"];
-    [moveLabel setTextAlignment:NSTextAlignmentCenter];
-    [moveLabel setTextColor:[UIColor whiteColor]];
-    [moveLabel setFont:[UIFont systemFontOfSize:18]];
-    
-    [viewController.view addSubview:moveLabel];
+    if (viewController.view.subviews.count > 1) { // bug fabrik
+        UIView *plCropOverlay = [[[viewController.view.subviews objectAtIndex:1]subviews] objectAtIndex:0];
+        plCropOverlay.hidden = YES;
+        int position = (screenHeight - screenWidth + 10)/2;
+        
+        CAShapeLayer *circleLayer = [CAShapeLayer layer];
+        UIBezierPath *path2 = [UIBezierPath bezierPathWithOvalInRect:
+                               CGRectMake(0.0f, position, screenWidth, screenWidth)];
+        [path2 setUsesEvenOddFillRule:YES];
+        
+        [circleLayer setPath:[path2 CGPath]];
+        
+        [circleLayer setFillColor:[[UIColor clearColor] CGColor]];
+        UIBezierPath *path = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(0, 0, screenWidth, screenHeight-72) cornerRadius:0];
+        
+        [path appendPath:path2];
+        [path setUsesEvenOddFillRule:YES];
+        
+        CAShapeLayer *fillLayer = [CAShapeLayer layer];
+        fillLayer.path = path.CGPath;
+        fillLayer.fillRule = kCAFillRuleEvenOdd;
+        fillLayer.fillColor = [UIColor blackColor].CGColor;
+        fillLayer.opacity = 0.8;
+        [viewController.view.layer addSublayer:fillLayer];
+        
+        UILabel *moveLabel = [[UILabel alloc]initWithFrame:CGRectMake(0, 10, screenWidth, 50)];
+        [moveLabel setText:@"Move and Scale"];
+        [moveLabel setTextAlignment:NSTextAlignmentCenter];
+        [moveLabel setTextColor:[UIColor whiteColor]];
+        [moveLabel setFont:[UIFont systemFontOfSize:18]];
+        
+        [viewController.view addSubview:moveLabel];
+    }
 }
 
 @end
